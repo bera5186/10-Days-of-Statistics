13e0801d3a48a5a779668a47259f479ab97ff2af
_rahul_._ai <35560689+bera5186@users.noreply.github.com>
2018-12-21 15:56:28 +0530

    Update Day 0: Mean, Median and Mode.cpp

--- Day 0: Mean, Median and Mode.cpp
+++ Day 0: Mean, Median and Mode.cpp
@@ -1,1 +1,31 @@
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm
+6 using namespace std;
+
+8 using namespace std;
+9 int main()
+10 int mean = 0, size, temp, median;
+cin>>size;
+vector int>a(size);
+
+for(int i=0;i<size; i++)
+cin>>temp;
+a. push_back (temp);
+mean += temp;
+
+mean = mean 2;
+sort(a.begin(), a.end());
+if(size 2 == 0)
+int numl = size/2;
+int num2 = numl + 1;
+median = a[num] + a[num2])/2;
+else{
+medium = a[size % 2];
+
+}
+
+return 0;
